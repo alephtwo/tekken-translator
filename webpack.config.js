fc72f67942eb522075573e650bdd46eb04a4e9b5
Ben Christianson <ben.christianson@forteresearch.com>
2019-01-04 14:22:08 -0600

    Add postcss

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,8 @@ const css = {
   use: ExtractTextPlugin.extract({
     fallback: 'style-loader',
     use: [
-      { loader:'css-loader', options: { modules: true } }
+      { loader: 'css-loader', options: { modules: true } },
+      'postcss-loader'
     ]
   })
 }
